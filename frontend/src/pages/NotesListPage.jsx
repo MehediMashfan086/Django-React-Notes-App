0299760a920df1c73a10dc73512b5b5a2e318110
Mehedi Hasan <mehedihasan62086@gmail.com>
2023-08-31 14:27:28 +0600

    making API calls and CORS fixed

--- frontend/src/pages/NotesListPage.jsx
+++ frontend/src/pages/NotesListPage.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const NotesListPage = () => {
+    let [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    let getNotes = async () => {
+        let response = await fetch("http://127.0.0.1:8000/api/notes/")
+        let data = await response.json()
+        setNotes(data)
+    }
+
   return (
-    <div>NotesListPage</div>
+    <div>
+        <div className="notes-list">
+            {
+                notes.map((notes, index) => (
+                    <h3 key={index}>{notes.body}</h3>
+                ))
+            }
+        </div>
+    </div>
   )
 }
 
